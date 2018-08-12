7f4399ecc7fd48ea4b212d2d3200fdf5e5721450
Jacob Gollub <jacobgollub@college.harvard.edu>
2018-08-12 19:31:30 -0400

    swapped with current version

--- guitar_app/venv/bin/activate.fish
+++ guitar_app/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jacobgollub/Desktop/guitar_app/venv"
+set -gx VIRTUAL_ENV "/Users/jacobgollub/Desktop/github_repos/guitar_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
